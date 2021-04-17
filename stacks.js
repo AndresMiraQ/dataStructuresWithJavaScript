31f0f64ebbf60d703156d43d3538dabf06c579ae
AndresMiraQ <krhistian1902@gmail.com>
2021-04-16 22:00:10 -0500

    STACKS

---
+++ stacks.js
@@ -0,0 +1,24 @@
+class MyStacks{
+    constructor(){
+        this.lenght = 0;
+        this.data = {};
+    }
+    //poner el elemento al final de la lista
+    push(item){
+        this.data[this.lenght] = item;
+        this.lenght++;
+        return this.data; 
+    }
+    //Eliminar el último elemento
+    pop(){
+        //Cúal es el elemento que se eliminará
+        const lastItem = this.data[this.lenght - 1];
+        //Eliminar elemento
+        delete this.data[this.lenght - 1];
+        this.lenght--;
+        return lastItem;
+    }
+    peek(){
+        return this.data[this.lenght - 1];
+    }
+}
